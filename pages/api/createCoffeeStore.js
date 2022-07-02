4c581408f811a330d0574c283cc732d947f66490
fenyrmaster <brandony2004@gmail.com>
2022-07-02 12:44:47 -0500

    Added more API functionality

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,48 +1,53 @@
-const Airtable = require('airtable');
-const base = new Airtable({apiKey: process.env.NEXT_PUBLIC_AIRTABLE_KEY}).base(process.env.NEXT_PUBLIC_AIRTABLE_BASE);
-
-const table = base("Coffee-stores");
+import { table, findRecord } from "../../lib/airtable";
 
 const createCoffeeStore = async (req, res) => {
-    try{
-        if(req.method === "POST"){
-            const findRecord = await table.select({
-                filterByFormula: `id=1254`
-            }).firstPage();
-    
-            if(findRecord.length > 0){
-                const data = findRecord.map(record => {
-                    return record.fields
-                })
-                res.status(200).json({
-                    status: "success",
-                    meesage: "Store found",
-                    data: data[0]
-                })
-            } else{
-                const store = await table.create([
-                    {
-                        fields: {
-                            id: "1254",
-                            Name: "some name",
-                            Neighbourhood: "Some neighbourhood",
-                            Adress: "some adress",
-                            Votes: 0,
-                            imgUrl: "some img url"
-                        }
+    if(req.method === "POST"){
+        const { id, Name, Adress, Neighbourhood, Votes, imgUrl } = req.body;
+        try{
+            if(id){
+                const records = await table.select({
+                    filterByFormula: `id=${id}`
+                }).firstPage();
+        
+                if(records.length > 0){
+                    const data = findRecord(records)
+                    res.status(200).json({
+                        status: "success",
+                        meesage: "Store found",
+                        data: data[0]
+                    })
+                } else{
+                    if(id && Name){
+                        const store = await table.create([
+                            {
+                                fields: {
+                                    id,
+                                    Name,
+                                    Neighbourhood,
+                                    Adress,
+                                    Votes,
+                                    imgUrl
+                                }
+                            }
+                        ]);
+                        const data = findRecord(store);
+                        res.status(201).json({
+                            message: "Store created",
+                            store: data[0]
+                        })
+                    } else{
+                        res.status(400).json({ message: "Name is missing" })
                     }
-                ]);
-                res.status(201).json({
-                    message: "Store created",
-                    store: store[0].fields
-                })
+                }
+            } else{
+                res.status(400).json({ message: "Id is required" })
             }
         }
-    } catch(error){
-        console.log(error);
-        res.json({ message: "Something went wrong", data: error });
+        catch(error){
+            console.log(error);
+            res.json({ message: "Something went wrong", data: error });
+        }
     }
-
 }
 
 export default createCoffeeStore;
